16ff57de5ecc43d104b23d832829191ac4e473aa
AndyTwoFish <twoyu_lin@gmail.com>
2025-06-01 19:45:55 +0800

    .

--- hello.py
+++ hello.py
@@ -1,3 +1,11 @@
 for i in range(5):
     print("Hello, World!")
     print("Andy")
+import os
+
+folder_path = "D:\\Andy\\andy_python"  
+files = os.listdir(folder_path)  
+
+file_list = [f for f in files if os.path.isfile(os.path.join(folder_path, f))]
+print(file_list)
+
